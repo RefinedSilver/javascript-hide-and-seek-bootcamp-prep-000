c8a0a6d89eda0a4d4d6804f1238db5e34eb845db
Ian McKenzie <imckenzie777@gmail.com>
2020-04-11 02:52:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const test = document.querySelector(selector);
+// const test = document.querySelector(selector);
 let getFirstSelector = function(selector) {
   // for (let i = 0; i < selector.length; i++) {
   //   selector[i].innerHTML = (i + 1).toString
