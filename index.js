5ef8fbb0dcbc1d2d2cf1273aee87ff4c627c8eea
Ian McKenzie <imckenzie777@gmail.com>
2020-04-11 03:13:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-
+let getFirstSelector = function(selector) {
+  return document.querySelector(selector);
+}
+let nestedTarget = () => {
+  return document.getElementById('nested').querySelector('div.target')
+}
+let deepestChild = function() {
+  let deep = document.getElementById('grand-node').querySelectorAll('div')
+  return deep[deep.length-1];
+}
+let increaseRankBy = function(n) {
+const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (let i = 0; i < upRank.length; i++) {
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
+  }
+}
