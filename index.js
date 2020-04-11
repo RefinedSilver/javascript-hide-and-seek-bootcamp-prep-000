5783021c429b759c02997c78fb43b1c72c611b97
Ian McKenzie <imckenzie777@gmail.com>
2020-04-11 02:59:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-
-// const test = document.querySelector(selector);
 let getFirstSelector = function(selector) {
-  // for (let i = 0; i < selector.length; i++) {
-  //   selector[i].innerHTML = (i + 1).toString
   return document.querySelector(selector);
-  }
-	// let li = document.querySelector("selector");
 }
 let nestedTarget = () => {
-
+  return document.getElementById('nested').querySelector('div.target')
 }
 
 let increaseRankBy = function(n) {
-
+const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list');
+  for (let i = 0; i < upRank.length; i++) {
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
+  }
 }
